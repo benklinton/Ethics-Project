13dd2714cd760648bc1310b8cec67c7c4f6a44df
Ben Linton <benklinton@gmail.com>
2022-04-05 11:21:48 -0600

    added more to header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,6 +3,7 @@ import "./style.css";
 import { Container } from "react-bootstrap";
 import { Row } from "react-bootstrap";
 import { Col } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 function Header() {
     return (
@@ -10,7 +11,12 @@ function Header() {
             <Container className="h-50">
                 <Row className="h-100">
                     <Col>
-                    <h1 className="mb-4 center text-black">Insert Text here</h1>
+                    <h1 className="mb-4 header-text">All your ethical needs, all in one place</h1>
+                    <h4 className="subheading-text">Ethics Inc provides the goods and services you need to live your best possible life</h4>
+                    <div className="center-buttons">
+                                <Button className='header-button button-color'>Check out Products</Button>
+                                <Button className="header-button-margin header-button button-color">Browse Services</Button>
+                            </div>
                     </Col>
                 </Row>
             </Container>
